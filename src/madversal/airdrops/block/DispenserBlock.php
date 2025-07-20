3bfe2b08e7cc1051732d7789a16fee33be59564f
Zuriel V. Alejandro <zuriivalentin@gmail.com>
2025-07-20 08:05:57 -0600

    Create DispenserBlock.php

---
+++ src/madversal/airdrops/block/DispenserBlock.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace madversal\airdrops\block;
+
+//dependencias por aca.
+
+use pocketmine\block\Block;
+use pocketmine\block\BlockTypeIds;
+use pocketmine\block\Solid;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\player\Player;
+use pocketmine\tile\Tile;
+
+
+class DispenserBlock extends Solid
+{
+
+}
