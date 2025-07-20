cb9d15b2adb7e6cf52a65d53d6c9311c0fef6d80
Zuriel V. Alejandro <zuriivalentin@gmail.com>
2025-07-20 07:56:32 -0600

    Create Main.php

---
+++ src/madversal/airdrops/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace madversal\airdrops;
+
+//aca se agregaran mas dependencias, pero luego las agregare.
+
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\block\Block;
+use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+use pocketmine\tile\Tile;
+use pocketmine\utils\Config;
+use pocketmine\utils\SingletonTrait;
+use pocketmine\utils\TextFormat;
+use pocketmine\world\Position;
+
+class Main extends PluginBase implements Listener
+{
+  
+}
