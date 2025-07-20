6bcf9e5bfc740baafa59b962efb7551d9b81b8a9
Zuriel V. Alejandro <zuriivalentin@gmail.com>
2025-07-20 08:02:40 -0600

    Create AirDropCommand.php

---
+++ src/madversal/airdrops/command/AirDropCommand.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace madversal\airdrops\command;
+
+use madversal\airdrops\utils\Utils;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+
+class AirDropCommand extends Command
+{
+
+}
