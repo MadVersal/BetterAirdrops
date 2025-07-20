7a0d417d4a47d5b294906f1f878028dde339b46e
Zuriel V. Alejandro <zuriivalentin@gmail.com>
2025-07-20 08:00:02 -0600

    Create Utils.php

---
+++ src/madversal/airdrops/utils/Utils.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace madversal\airdrops\utils;
+
+//igual agregare mas dependencias pronto.
+
+use pocketmine\block\Block;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+
+final class Utils
+{
+//por aqui se registraran todos los menus.
+}
